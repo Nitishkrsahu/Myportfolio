799a767255416908a5d6307199b3283f998d0e99
Nitish <nitishkrsahu123@gmail.com>
2025-06-26 16:13:36 +0530

    resume download is fixed

--- myportfolio/src/components/Footer/Footer.jsx
+++ myportfolio/src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <footer className="container-fluid">
+    <footer className="container-fluid border border-1 border-bottom-0 border-start-0 border-end-0 border-dark pt-1 pb-lg-1">
       <div className="row" style={{ color: '#FE0087' }}>
         <div className="d-flex gap-3 col-lg-6 justify-content-center align-items-center mb-1">
           <div className="btn btn-dark p-2 d-flex justify-content-center align-items-center rounded rounded-1" 
@@ -30,7 +30,9 @@ export default function Footer() {
           </div>
         </div>
         <div className="text-center col-lg-6">
-          <p className="fw-bold">&copy; {new Date().getFullYear()} MyWebsite. All rights reserved.</p>
+          <p className="fw-bold p-0 m-0" style={{
+            color: '#9C27B0'
+          }}><img src="/footer/copyright.svg" alt="copyright-icon" width={20}/> {new Date().getFullYear()} MyWebsite. All rights reserved.</p>
         </div>
       </div>
     </footer>
--- myportfolio/src/components/button-component/resume.jsx
+++ myportfolio/src/components/button-component/resume.jsx
@@ -1,13 +1,17 @@
-export const Resume = ()=>{
+import { Link } from "react-router"
+
+export const Resume = () => {
     return (
         <div className="d-flex gap-2">
-            <button className="btn btn-outline-dark px-3">
-                <span className="text-white">Download CV </span>
-                 <img src="/resume/download.svg" alt="download-icon" />
-            </button>
+            <a href="/Nitish_CV_UI_FULL_STACK_2025.pdf" className="text-decoration-none">
+                <button className="btn btn-outline-dark px-3">
+                    <span className="text-white">Download CV </span>
+                    <img src="/resume/download.svg" alt="download-icon" />
+                </button>
+            </a>
             <button className="btn btn-outline-warning px-3">
-                <span>About </span>
-                <img src="/resume/info.svg" alt="info-icon" />
+                <Link to={"/about"} className="text-decoration-none text-white">About Us </Link>
+                <img src="/resume/info.svg" alt="info-icon" height={20} />
             </button>
         </div>
     )
